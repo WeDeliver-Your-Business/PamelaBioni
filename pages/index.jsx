af007b22064065077dedce43a3da61f986bc61d9
RuanL-Dev <ruanlopes21@gmail.com>
2023-07-02 11:41:54 -0300

    :sparkles: Added new Contact Page component for MainPart6
    ContactPage created to attend MainPart6 task from clickup and according figma

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import FrontPageImage from '../src/layout/frontPageImage/FrontPageImage'
 import ProfessionalProfile from '../src/layout/professionalProfile/ProfessionalProfile'
 import TopicsAreas from '../src/layout/topicsAreas/TopicsAreas'
 import CarePage from '../src/layout/carePage/CarePage'
+import ContactPage from '../src/layout/contactPage/ContactPage'
 
 function MainPage() {
   return (
@@ -14,6 +15,7 @@ function MainPage() {
       <ProfessionalProfile />
       <TopicsAreas />
       <CarePage />
+      <ContactPage />
     </>
   )
 }
---
+++ src/layout/contactPage/ContactPage.jsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+import H4 from '../../typograph/H4'
+
+import IconImage from '../../utils/iconImage/IconImage'
+
+const ColouredContainer = styled.div`
+  background-color: ${(props) => props.theme.primary};
+  width: 100%;
+  border-radius: 30px 0px 0px 0px;
+`
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.ColorWhite};
+  border-radius: 20px 100px 0px 0px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  padding-bottom: 100px;
+`
+
+const ContainerArrow = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const StyledArrow = styled.div`
+  padding-bottom: 25px;
+`
+const LineColour = styled.div`
+  background-color: ${(props) => props.theme.primary};
+  box-sizing: border-box;
+  padding: 0.5px 0;
+`
+
+function ContactPage() {
+  return (
+    <>
+      <ColouredContainer>
+        <Container>
+          <ContainerArrow>
+            <StyledArrow>
+              <IconImage imageName="ArrowDown" type="png" size="40px" />
+            </StyledArrow>
+          </ContainerArrow>
+          <LineColour />
+        </Container>
+      </ColouredContainer>
+    </>
+  )
+}
+
+export default ContactPage
