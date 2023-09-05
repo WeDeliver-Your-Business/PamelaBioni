c3976e9b85d01696cb7a00b3eae79ba5672effde
Bruno Patrício <bpatriciocosta@gmail.com>
2023-09-05 14:45:28 -0300

    Teste 5

--- src/layout/carePage/CarePage.jsx
+++ src/layout/carePage/CarePage.jsx
@@ -250,7 +250,7 @@ function CarePage() {
               <ImageCardHelp>
                 <StyledImageCard>
                   <a
-                    href="https://api.whatsapp.com/send?phone=556192392319&text=Olá,%20Dra.%20Pâmela"
+                    href="https://api.whatsapp.com/send?phone=555193524000&text=Olá,%20Dra.%20Pâmela"
                     target="_blank"
                     rel="noreferrer"
                   >
